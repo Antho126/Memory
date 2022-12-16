a3d6bf138bb38b37c02a3e60f95801f4662e6546
Anthony Ruiz <anthonyruizpro126@gmail.com>
2022-12-16 16:01:09 +0100

    ajout de addEventListener aux cartes

--- script.js
+++ script.js
@@ -3,6 +3,5 @@ let section = document.getElementById("memory");
 let memory = new Memory(3,8);
 section.append(memory.toHTML());
 
-let y = 10000;
-console.log(--y);
+console.log(memory.at(1,4));
 // document.body.append(i.toHTML());
